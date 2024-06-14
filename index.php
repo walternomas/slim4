e5dd5f8e8428a4aa3031cdcdc5dd635b102dcba4
Walter Barrios <walter.barrios@nerdo.solutions>
2024-06-14 11:58:57 -0300

    init slim

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+use Slim\Factory\AppFactory;
+
+require __DIR__ . "/vendor/autoload.php";
+
+$app = AppFactory::create();
+
+$app->get('/', function ($request, $response, $args) {
+    $response->getBody()->write("Hello World!");
+    return $response;
+});
+
+$app->run();
