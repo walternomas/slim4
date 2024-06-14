820ef90b43290f5a991856d60b2cf3ed2b71e1ec
Walter Barrios <walter.barrios@nerdo.solutions>
2024-06-14 12:49:31 -0300

    Add twig

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
 
+use Slim\Views\Twig;
 use Slim\Factory\AppFactory;
 use App\Controllers\ContactController;
 use App\Controllers\BlogController as Blog;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . "/vendor/autoload.php";
 
 $app = AppFactory::create();
+
+$twig = Twig::create('templates', ['cache' => false]);
+
+$app->add(TwigMiddleware::create($app, $twig));
 
 $app->get('/', 'App\Controllers\HomeController:index');
 
--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -3,12 +3,18 @@ namespace App\Controllers;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
 
 class HomeController
 {
     function index(Request $request, Response $response, array $args)
     {
-        $response->getBody()->write("Welcome to Home!");
-        return $response;
+        $view = Twig::fromRequest($request);
+        $data = [
+            'title' => 'Principal Site',
+            'h1' => 'Welcome Home!'
+        ];
+
+        return $view->render($response, "index.html", $data);
     }
 }
