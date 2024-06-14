14d674a2ccaaaf10ecfa3951eba922c63885deba
Walter Barrios <walter.barrios@nerdo.solutions>
2024-06-14 12:11:38 -0300

    first get routes

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . "/vendor/autoload.php";
 
 $app = AppFactory::create();
 
-$app->get('/', function ($request, $response, $args) {
+$app->get('/', function (Request $request, Response $response, array $args) {
     $response->getBody()->write("Hello World!");
     return $response;
+});
+
+$app->get('/contact', function (Request $request, Response $response, array $args) {
+   $response->getBody()->write("Contact!");
+   return $response;
 });
 
 $app->run();
